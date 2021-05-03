9c8e933cf5299488b798e62229f3e3e50e809777
Pradeep M Kodavoor <pkodavoo@cisco.com>
2021-05-03 19:34:54 +0530

    Add Match Rest API

--- src/main/java/com/practice/ipldashboard/controller/TeamController.java
+++ src/main/java/com/practice/ipldashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.practice.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.practice.ipldashboard.model.Match;
 import com.practice.ipldashboard.model.Team;
 import com.practice.ipldashboard.repository.MatchRepository;
 import com.practice.ipldashboard.repository.TeamRepository;
@@ -8,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +32,16 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
 
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year+1, 1, 1);
+
+        // return matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, startDate, endDate, teamName, startDate, endDate);
+        return matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 }
